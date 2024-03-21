b761fc5c9c590f8d21942311b8108c3da90dc0df
Argos Stone <peter@raedangroup.com>
2024-03-21 02:49:29 -0400

    ex 6.3 v1

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -9,7 +9,7 @@ const apiUrl = 'https://percys-picks-a2286948842a.herokuapp.com/';
 @Injectable({
   providedIn: 'root'
 })
-export class ApiEndpointService {
+export class FetchApiDataService {
   // Inject the HttpClient module to the constructor params
  // This will provide HttpClient to the entire class, making it available via this.http
   constructor(private http: HttpClient) {
